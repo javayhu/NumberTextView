f8e66b73ef711d6921f44046d9d47b79cbec0ed1
javayhu <javayhu@tencent.com>
2017-10-26 14:42:38 +0800

    update version 3.0

--- app/src/main/java/me/javayhu/numbertextview/MainActivity.java
+++ app/src/main/java/me/javayhu/numbertextview/MainActivity.java
@@ -1,61 +1,89 @@
 package me.javayhu.numbertextview;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.app.AppCompatDelegate;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.LinearLayout;
 
 import me.javayhu.lib.NumberTextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private LinearLayout mContentContainer;
+    private NumberTextView mNumberTextView1;
+    private NumberTextView mNumberTextView2;
+    private NumberTextView mNumberTextView3;
 
-    private NumberTextView mNumberTextView;
-    private int mClickCount = -1;
-    private boolean[] mOperations = new boolean[]{true, true, true, true, false, false, false, false};
-    //private int[] mColors = new int[]{Color.CYAN, Color.GREEN, Color.BLUE, Color.RED, Color.DKGRAY, Color.MAGENTA, Color.BLACK};
-
-    private int mClickCount2 = -1;
+    private boolean isNight = false;
+    private SharedPreferences mSharedPreferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mContentContainer = (LinearLayout) findViewById(R.id.content);
+        mSharedPreferences = getSharedPreferences("demo", MODE_PRIVATE);
+        isNight = mSharedPreferences.getBoolean("night", false);
 
-        mNumberTextView = (NumberTextView) findViewById(R.id.numberTextView);//这个NumberTextView会使用第二个构造方法
-        mNumberTextView.setCount(98);
-        mNumberTextView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ++mClickCount;
-                if (mOperations[mClickCount % mOperations.length]) {
-                    mNumberTextView.plusOne();
-                } else {
-                    mNumberTextView.minusOne();
-                }
-                //mNumberTextView.setTextColor(mColors[mClickCount % mColors.length]);//测试颜色变化
-            }
-        });
+        mNumberTextView1 = (NumberTextView) findViewById(R.id.numberTextView1);
+        mNumberTextView1.setCount(98);
+        mNumberTextView1.setOnClickListener(new NumberClickListener());
 
-        //final NumberTextView numberTextView2 = new NumberTextView(this, null, 0, R.style.NumberTextViewStyle);//这个NumberTextView会使用第四个构造方法，但是在Android 5.0及以下版本的系统中会crash
-        final NumberTextView numberTextView2 = new NumberTextView(this, null, R.style.Widget_NumberTextViewStyle);//这个NumberTextView会使用第三个构造方法
-        numberTextView2.setCount(998);
-        numberTextView2.setTextSize(60);
-        numberTextView2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ++mClickCount2;
-                if (mOperations[mClickCount2 % mOperations.length]) {
-                    numberTextView2.plusOne();
-                } else {
-                    numberTextView2.minusOne();
-                }
-            }
-        });
-        mContentContainer.addView(numberTextView2, new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        mNumberTextView2 = (NumberTextView) findViewById(R.id.numberTextView2);
+        mNumberTextView2.setCount(998);
+        mNumberTextView2.setOnClickListener(new NumberClickListener());
+
+        mNumberTextView3 = (NumberTextView) findViewById(R.id.numberTextView3);
+        mNumberTextView3.setCount(9998);
+        mNumberTextView3.setOnClickListener(new NumberClickListener());
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.changeTheme) {
+            changeTheme();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void changeTheme() {
+        if (isNight) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            mSharedPreferences.edit().putBoolean("night", false).commit();
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            mSharedPreferences.edit().putBoolean("night", true).commit();
+        }
+        recreate();
+    }
+
+    private class NumberClickListener implements View.OnClickListener {
+
+        private int mClickCount = -1;
+        private boolean[] mOperations = new boolean[]{true, true, true, true, false, false, false, false};
+
+        @Override
+        public void onClick(View v) {
+            if (!(v instanceof NumberTextView)) {
+                return;
+            }
+            NumberTextView numberTextView = (NumberTextView) v;
+            ++mClickCount;
+            if (mOperations[mClickCount % mOperations.length]) {
+                numberTextView.plusOne();
+            } else {
+                numberTextView.minusOne();
+            }
+        }
+    }
+
 }
